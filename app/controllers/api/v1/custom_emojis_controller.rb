550a0fa08a9e649a464a5e5c3e0f40ce3f40431a
Jeremy Kescher <jeremy@kescher.at>
2022-07-05 09:49:31 +0200

    Exclude /api/v1/custom_emojis from secure fetch (unless allowlist mode is enabled)

--- app/controllers/api/v1/custom_emojis_controller.rb
+++ app/controllers/api/v1/custom_emojis_controller.rb
@@ -2,6 +2,7 @@
 
 class Api::V1::CustomEmojisController < Api::BaseController
   skip_before_action :set_cache_headers
+  skip_before_action :require_authenticated_user!, unless: :whitelist_mode?
 
   def index
     expires_in 3.minutes, public: true
