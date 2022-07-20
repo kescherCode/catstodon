df02b6629527f7ff64bde1d4d3033d8d694a4b88
Jeremy Kescher <jeremy@kescher.at>
2022-07-20 18:03:40 +0200

    Allow adjusting avatar sizes (local and remote) via config

--- app/models/concerns/account_avatar.rb
+++ app/models/concerns/account_avatar.rb
@@ -4,7 +4,8 @@ module AccountAvatar
   extend ActiveSupport::Concern
 
   IMAGE_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'].freeze
-  LIMIT = 2.megabytes
+  LOCAL_LIMIT = (ENV['MAX_AVATAR_SIZE'] || 2.megabytes).to_i
+  LIMIT       = [LOCAL_LIMIT, (ENV['MAX_REMOTE_AVATAR_SIZE'] || 2.megabytes).to_i].max
 
   class_methods do
     def avatar_styles(file)
@@ -20,7 +21,8 @@ module AccountAvatar
     # Avatar upload
     has_attached_file :avatar, styles: ->(f) { avatar_styles(f) }, convert_options: { all: '-strip' }, processors: [:lazy_thumbnail]
     validates_attachment_content_type :avatar, content_type: IMAGE_MIME_TYPES
-    validates_attachment_size :avatar, less_than: LIMIT
+    validates_attachment_size :avatar, less_than: LIMIT, unless: :local?
+    validates_attachment_size :avatar, less_than: LOCAL_LIMIT, if: :local?
     remotable_attachment :avatar, LIMIT, suppress_errors: false
   end
 
