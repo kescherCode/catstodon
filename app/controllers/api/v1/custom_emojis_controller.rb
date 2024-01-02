b18cd3f4bde8826cfef48aa8ad71240caf7e3e3f
Jeremy Kescher <jeremy@kescher.at>
2024-01-02 13:55:07 +0100

    fix: check for limited_federation_mode instead of removed whitelist_mode

--- app/controllers/api/v1/custom_emojis_controller.rb
+++ app/controllers/api/v1/custom_emojis_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::V1::CustomEmojisController < Api::BaseController
   vary_by '', unless: :disallow_unauthenticated_api_access?
-  skip_before_action :require_authenticated_user!, unless: :whitelist_mode?
+  skip_before_action :require_authenticated_user!, unless: :limited_federation_mode?
 
   def index
     cache_even_if_authenticated! unless disallow_unauthenticated_api_access?
